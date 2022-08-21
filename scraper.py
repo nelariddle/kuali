2aa7aa9d57992a2a09d3069335f5f1408157ddf7
Nela Riddle <62609098+nelariddle@users.noreply.github.com>
2022-08-21 19:53:59 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,7 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-# PATH = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://apps.iu.edu/kpme-prd/Clock.do")
 
